2ece3094fb86147453fc3875b82dbb0be36ec277
leg1771 <leg1770@naver.com>
2023-07-31 22:23:30 +0900

    로그인 상태관리 위한 swr 코드 우선 입력, 토큰 쿠키로 저장하기로 하고 리액트 쿠키 설치 후 관련코드 작성중

--- healthcare/src/pages/Login.tsx
+++ healthcare/src/pages/Login.tsx
@@ -1,7 +1,64 @@
 import useSWR from 'swr'
-
+import React, { useState, useCallback } from 'react';
+import axios from 'axios'
+import fetcher from '../utils/fetcher';
 
 export default function Login(){
+
+    //로그인 기능 코드
+
     
+
+        // 로그인 후 가져온 데이터를 전역으로 관리하기 위한 swr 코드 
+    const { data, error, mutate } = useSWR('/api/users', fetcher);
+    const [loginError, setLoginError] = useState(false);
+    const [id, setId] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const onSubmit = useCallback(
+      (e) => {
+        e.preventDefault();
+        setLoginError(false);
+        axios
+          .post(
+            'http://223.130.161.221/api/v1/admins/login',
+            { id, password },
+            {
+              withCredentials: true,
+            },
+          )
+          .then(() => {
+            mutate()
+          })
+          .catch((error) => {
+            setLoginError(error.response?.data?.statusCode === 401);
+          });
+      },
+      [id, password],
+    );
+    
+   
+
+    const onChangeEmail = (e)=>{
+        setId(e.target.value)
+        
+    }
+
+    const onChangePassword = (e)=>{
+        setPassword(e.target.value)
+       
+    }
+
+
+    
+    return (
+        <>
+            <div>
+                id : <input type = 'id' onChange = {onChangeEmail} ></input>
+                pw : <input type = 'password' onChange = {onChangePassword}></input>
+                <button onClick = {onSubmit}>로그인</button>
+            </div>
+        </>
+    )
 }
 
