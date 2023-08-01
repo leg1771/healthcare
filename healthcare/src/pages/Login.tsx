4c4f3d8cb9ca7845a89d4f434b286609c36e2d93
leg1771 <leg1770@naver.com>
2023-08-02 00:13:54 +0900

    post에 header 추가하여 로그인하고 토큰 받아옴

--- healthcare/src/pages/Login.tsx
+++ healthcare/src/pages/Login.tsx
@@ -2,27 +2,27 @@ import useSWR from 'swr'
 import React, { useState, useCallback } from 'react';
 import axios from 'axios'
 import fetcher from '../utils/fetcher';
+import { useCookies } from "react-cookie";
 
 export default function Login(){
 
     //로그인 기능 코드
-
     
-
+   
         // 로그인 후 가져온 데이터를 전역으로 관리하기 위한 swr 코드 
-    const { data, error, mutate } = useSWR('/api/users', fetcher);
+    const { data, error, mutate } = useSWR(‘/api/users’, fetcher);
     const [loginError, setLoginError] = useState(false);
     const [id, setId] = useState('');
     const [password, setPassword] = useState('');
   
-    const onSubmit = useCallback(
+    const onSubmit = useCallback( // 
       (e) => {
         e.preventDefault();
         setLoginError(false);
         axios
           .post(
             'http://223.130.161.221/api/v1/admins/login',
-            { id, password },
+            { id, password },   
             {
               withCredentials: true,
             },
@@ -49,6 +49,25 @@ export default function Login(){
        
     }
 
+    const loginSubmitHandler = (e) => {
+        
+
+        axios.post('http://223.130.161.221/api/v1/admins/login',null,{
+            headers :  {
+                'Authorization' : 'dGVhbTIxOnRlYW0yMSEh'
+            }
+        })
+        .then(res => { // headers: {…} 로 들어감.
+          console.log('send ok', res.data)
+          mutate()
+        })
+
+        
+     
+
+}
+
+
 
     
     return (
@@ -56,9 +75,15 @@ export default function Login(){
             <div>
                 id : <input type = 'id' onChange = {onChangeEmail} ></input>
                 pw : <input type = 'password' onChange = {onChangePassword}></input>
-                <button onClick = {onSubmit}>로그인</button>
+                <button onClick = {loginSubmitHandler} >로그인</button>
             </div>
         </>
     )
+
+
+
+
+
 }
+
 
